f601e6aabfcbd5a09661a0e2cb05bb75147cd71f
Hoang Viet <thvroyal@gmail.com>
2021-08-06 14:44:45 +0700

    feat: form search for file management

--- src/views/FileManagement/index.vue
+++ src/views/FileManagement/index.vue
@@ -3,6 +3,34 @@
     <nids-header />
     <c-box mt="60px" bg="gray.50">
       <statistic />
+      <CFlex align="center" justify="between" px="32px">
+        <c-stack :spacing="2" isInline>
+          <c-input-group>
+            <c-input-left-element
+              ><c-icon name="search" color="gray.300"
+            /></c-input-left-element>
+            <c-input type="text" placeholder="File name" />
+          </c-input-group>
+          <c-input-group>
+            <c-input-left-element
+              ><c-icon name="search" color="gray.300"
+            /></c-input-left-element>
+            <c-input type="text" placeholder="Upload time" />
+          </c-input-group>
+          <c-input-group>
+            <c-input-left-element
+              ><c-icon name="search" color="gray.300"
+            /></c-input-left-element>
+            <c-input type="text" placeholder="Start time" />
+          </c-input-group>
+          <c-input-group>
+            <c-input-left-element
+              ><c-icon name="search" color="gray.300"
+            /></c-input-left-element>
+            <c-input type="text" placeholder="Time" />
+          </c-input-group>
+        </c-stack>
+      </CFlex>
     </c-box>
   </c-box>
 </template>
